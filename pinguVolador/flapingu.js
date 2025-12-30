8522d3706c43fa426cb61eb1543c80a492ab9075
Clara Jenhee <clara20franco@gmail.com>
2025-12-30 13:13:24 -0700

    menus añadidos

--- pinguVolador/flapingu.js
+++ pinguVolador/flapingu.js
@@ -1,3 +1,65 @@
+const menu = document.getElementById("another-games");
+const icon = document.getElementById("menu-icon");
+
+let offsetX, offsetY, isDragging = false;
+
+// --- Drag con mouse ---
+menu.addEventListener("mousedown", (e) => {
+  if (e.target.closest("#menu-icon")) return; // no arrastrar si clic en icono
+  isDragging = true;
+  offsetX = e.clientX - menu.offsetLeft;
+  offsetY = e.clientY - menu.offsetTop;
+});
+
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    menu.style.left = (e.clientX - offsetX) + "px";
+    menu.style.top = (e.clientY - offsetY) + "px";
+  }
+});
+
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+// --- Drag con touch ---
+menu.addEventListener("touchstart", (e) => {
+  if (e.target.closest("#menu-icon")) return;
+  isDragging = true;
+  const touch = e.touches[0];
+  offsetX = touch.clientX - menu.offsetLeft;
+  offsetY = touch.clientY - menu.offsetTop;
+});
+
+document.addEventListener("touchmove", (e) => {
+  if (isDragging) {
+    const touch = e.touches[0];
+    menu.style.left = (touch.clientX - offsetX) + "px";
+    menu.style.top = (touch.clientY - offsetY) + "px";
+  }
+});
+
+document.addEventListener("touchend", () => {
+  isDragging = false;
+});
+
+// --- Toggle expand/collapse ---
+icon.addEventListener("click", () => {
+  menu.classList.toggle("expanded");
+
+  if (menu.classList.contains("expanded")) {
+    // SVG lleno (abierto)
+    icon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#e1ff00" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-device-gamepad-3"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 12l-3 -3h-2a1 1 0 0 0 -1 1v4a1 1 0 0 0 1 1h2z" /><path d="M15 12l3 -3h2a1 1 0 0 1 1 1v4a1 1 0 0 1 -1 1h-2z" /><path d="M12 15l-3 3v2a1 1 0 0 0 1 1h4a1 1 0 0 0 1 -1v-2z" /><path d="M12 9l-3 -3v-2a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v2z" /></svg>
+    `;
+  } else {
+    // SVG outline (cerrado)
+    icon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#e1ff00" class="icon icon-tabler icons-tabler-filled icon-tabler-device-gamepad-3"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12.707 14.293l3 3a1 1 0 0 1 .293 .707v2a2 2 0 0 1 -2 2h-4a2 2 0 0 1 -2 -2v-2a1 1 0 0 1 .293 -.707l3 -3a1 1 0 0 1 1.414 0m-6.707 -6.293a1 1 0 0 1 .707 .293l3 3a1 1 0 0 1 0 1.414l-3 3a1 1 0 0 1 -.707 .293h-2a2 2 0 0 1 -2 -2v-4a2 2 0 0 1 2 -2zm14 0a2 2 0 0 1 2 2v4a2 2 0 0 1 -2 2h-2a1 1 0 0 1 -.707 -.293l-3 -3a1 1 0 0 1 0 -1.414l3 -3a1 1 0 0 1 .707 -.293zm-6 -6a2 2 0 0 1 2 2v2a1 1 0 0 1 -.293 .707l-3 3a1 1 0 0 1 -1.414 0l-3 -3a1 1 0 0 1 -.293 -.707v-2a2 2 0 0 1 2 -2z" /></svg>
+    `;
+  }
+});
+
 (() => {
   const canvas = document.getElementById("game");
   const ctx = canvas.getContext("2d");
